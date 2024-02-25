ebc74c1e085835135797caf441b5ceb437acb884
Vaibhav Bergal <bergalvaibhav33@gmail.com>
2024-02-25 13:26:54 +0530

    Update Portfolio

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import { Container } from "./index";
 import JS from "../assets/javascript.svg";
 import ReactIcon from "../assets/react.svg";
@@ -8,24 +8,8 @@ import HTML from "../assets/html.svg";
 import Bootstrap from "../assets/bootstrap.svg";
 import Tailwind from "../assets/tailwind.svg";
 import CSS from "../assets/css.svg";
-import { motion } from "framer-motion";
-
-const fadeUp = {
-  open: { opacity: 1, y: 0, transition: { staggerChildren: 0.2, duration: 1 } },
-  closed: { opacity: 0, y: 60 },
-};
-const ZoomIn = {
-  open: {
-    opacity: 1,
-    scale: 1,
-    transition: { staggerChildren: 0.2, duration: 1 },
-  },
-  closed: { opacity: 0, scale: 0.6 },
-};
 
 const Skill = () => {
-  const scrollRef = useRef(null);
-
   const skills = [
     { id: 1, title: "HTML", img: HTML },
     { id: 2, title: "CSS", img: CSS },
@@ -39,28 +23,15 @@ const Skill = () => {
 
   return (
     <>
-      <Container id="skills" className="py-2" ref={scrollRef}>
-        <motion.h1
-          initial="closed"
-          whileInView="open"
-          viewport={{ root: scrollRef }}
-          variants={fadeUp}
-          className="py-1 text-2xl font-bold text-gray-400 md:py-2 md:text-4xl font-mooli"
-        >
+      <Container id="skills" className="py-2">
+        <h1 className="py-1 text-2xl font-bold text-gray-400 md:py-2 md:text-4xl font-mooli">
           Tech Stack
-        </motion.h1>
+        </h1>
         <hr className="border-black/30 dark:border-white/30" />
 
-        <motion.ul
-          initial="closed"
-          whileInView="open"
-          viewport={{ root: scrollRef }}
-          variants={ZoomIn}
-          className="flex flex-wrap justify-center gap-5 my-5 mt-8 md:mt-14"
-        >
+        <ul className="flex flex-wrap justify-center gap-5 my-5 mt-8 md:mt-14">
           {skills.map((item) => (
-            <motion.li
-              variants={ZoomIn}
+            <li
               key={item.id}
               className="flex items-center gap-3 p-2 px-3 font-mono font-semibold bg-gray-400 border-2 border-blue-600 rounded-lg shadow-lg cursor-pointer shadow-black/50 dark:text-white dark:border-red-600 dark:bg-black md:text-lg dark:even:border-purple-600 hover:scale-105"
             >
@@ -71,9 +42,9 @@ const Skill = () => {
                 className="w-8 mix-blend-multiply dark:mix-blend-normal"
               />
               <p>{item.title}</p>
-            </motion.li>
+            </li>
           ))}
-        </motion.ul>
+        </ul>
       </Container>
     </>
   );
