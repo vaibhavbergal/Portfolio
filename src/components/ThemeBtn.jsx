9e315b880e708a4755ecbbf306fbd2e57c14e513
Vaibhav Bergal <bergalvaibhav33@gmail.com>
2024-02-25 13:40:41 +0530

    Update Portfolio

--- src/components/ThemeBtn.jsx
+++ src/components/ThemeBtn.jsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 
 export default function ThemeBtn({ className, ...props }) {
   const { theme, lightTheme, darkTheme } = useTheme();
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(true);
   const ref = useRef();
 
   const handleTheme = () => {
@@ -28,10 +28,10 @@ export default function ThemeBtn({ className, ...props }) {
           onClick={handleTheme}
           className={`p-0 px-1 text-2xl bg-red border-none hover:text-amber-400 dark:text-white hover:red ${className} `}
         >
-          {isDark ? (
-            <MdOutlineDarkMode className="hover:text-amber-400" />
-          ) : (
+          {!isDark ? (
             <MdOutlineLightMode className="hover:text-amber-400" />
+          ) : (
+            <MdOutlineDarkMode className="hover:text-amber-400" />
           )}
         </Button>
       </motion.div>
